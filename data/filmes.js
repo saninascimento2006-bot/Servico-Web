2956b39b239833462489c3ee77a175970fbebb59
Anna AA <saninascimento2006@gmail.com>
2025-10-06 22:06:01 -0300

    subir paginas para web

---
+++ data/filmes.js
@@ -0,0 +1,53 @@
+export const filmes = [
+  {
+    id: "1",
+    titulo: "A espera de um milagre",
+    ano: 1999,
+    genero: "Drama",
+    nota: 9.2,
+    poster: "https://a-static.mlcdn.com.br/1500x1500/poster-cartaz-a-espera-de-um-milagre-pop-arte-poster/poparteskins2/15938515465/63942ae26a95e3fe4274addeefe0f366.jpeg"
+    // poster: "https://br.web.img2.acsta.net/medias/nmedia/18/91/66/66/20156966.jpg"
+  },
+  {
+    id: "2",
+    titulo: "Velozes e furiosos",
+    ano: 2001,
+    genero: "Ação",
+    nota: 8.6,
+    poster: "https://img.elo7.com.br/product/zoom/268DCC4/big-poster-velozes-e-furiosos-lo06-tamanho-90x60-cm-velozes-e-furiosos.jpg"
+  },
+  {
+    id: "3",
+    titulo: "500 dias com ela",
+    ano: 2009,
+    genero: "Romance",
+    nota: 8.5,
+    poster: "https://media.fstatic.com/oITLDWNd8w-0S761rOYkqkycUzE=/322x478/smart/filters:format(webp)/media/movies/covers/2011/06/daaf0bf2a5d45f274bd2c672aed35cce.jpg"
+    // poster: "https://br.web.img3.acsta.net/medias/nmedia/18/87/30/15/20028674.jpg"
+  },
+  {
+    id: "4",
+    titulo: "Click",
+    ano: 2006,
+    genero: "Comédia",
+    nota: 7.9,
+    poster: "https://macmagazine.com.br/wp-content/uploads/2013/09/19-click.jpg"
+    // poster: "https://br.web.img2.acsta.net/medias/nmedia/18/87/03/15/19871286.jpg"
+  },
+  {
+    id: "5",
+    titulo: "Corra",
+    ano: 2017,
+    genero: "Suspense",
+    nota: 8.4,
+    poster: "https://uauposters.com.br/media/catalog/product/3/5/353720210407-uau-posters-corra-get-out-filmes-4.jpg"
+  },
+  {
+    id: "6",
+    titulo: "Interestelar",
+    ano: 2014,
+    genero: "Sci-Fi",
+    nota: 9.2,
+    poster: "https://uauposters.com.br/media/catalog/product/7/0/706020150509-uau-posters-filmes-interestelar-interestellar-1.jpg"
+  }
+];
