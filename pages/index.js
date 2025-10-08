19f01c15eb4cdb9d21e2e5de1ccd006d0e5844df
Anna SS+ <saninascimento2006@gmail.com>
2025-10-08 20:18:05 -0300

    Adicionar lista de filme

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,63 @@
+import { filmes } from "../data/filmes.js"
+
 function Home () {
-    return <h1>Teste</h1>
+    console.log(filmes)
+
+const estiloImagem = {
+    width: "300px"
+}
+
+    return <><h1>Filmes</h1>
+
+    <ul>
+        <li>
+            <p>Título: {filmes[0].titulo}</p>
+            <p>Ano: {filmes[0].ano}</p>
+            <p>Gênero: {filmes[0].genero}</p>
+            <p>Nota: {filmes[0].nota}</p>
+
+            <img style= {estiloImagem} src= {filmes[0].poster}/>
+        </li>    
+        <li>
+            <p>Título: {filmes[1].titulo}</p>
+            <p>Ano: {filmes[1].ano}</p>
+            <p>Gênero: {filmes[1].genero}</p>
+            <p>Nota: {filmes[1].nota}</p>
+
+            <img style= {estiloImagem} src= {filmes[1].poster}/>
+        </li> 
+        <li>
+            <p>Título: {filmes[2].titulo}</p>
+            <p>Ano: {filmes[2].ano}</p>
+            <p>Gênero: {filmes[2].genero}</p>
+            <p>Nota: {filmes[2].nota}</p>
+
+            <img style= {estiloImagem} src= {filmes[2].poster}/>
+        </li> 
+        <li>
+            <p>Título: {filmes[3].titulo}</p>
+            <p>Ano: {filmes[3].ano}</p>
+            <p>Gênero: {filmes[3].genero}</p>
+            <p>Nota: {filmes[3].nota}</p>
+
+            <img style= {estiloImagem} src= {filmes[3].poster}/>
+            <li>
+            <p>Título: {filmes[4].titulo}</p>
+            <p>Ano: {filmes[4].ano}</p>
+            <p>Gênero: {filmes[4].genero}</p>
+            <p>Nota: {filmes[4].nota}</p>
+
+            <img style= {estiloImagem} src= {filmes[4].poster}/>
+        </li> 
+        <li>
+            <p>Título: {filmes[5].titulo}</p>
+            <p>Ano: {filmes[5].ano}</p>
+            <p>Gênero: {filmes[5].genero}</p>
+            <p>Nota: {filmes[5].nota}</p>
+
+            <img style= {estiloImagem} src= {filmes[5].poster}/>
+        </li> 
+        </li> 
+    </ul></>
 }
 export default Home;
