5e33f3a36265d393ac158974f65cde77ab0e4250
Anna SS+ <saninascimento2006@gmail.com>
2025-10-08 20:29:41 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,34 @@ import { filmes } from "../data/filmes.js"
 function Home () {
     console.log(filmes)
 
+  const estiloLista = {
+    display: "flex",
+    flexDirection: "row",
+    gap: "20px",
+    listStyle: "none",
+    padding: "20px",
+    margin: 0,
+    overflowX: "auto", 
+    whiteSpace: "nowrap",
+  };
+
+  const estiloItem = {
+    flex: "0 0 auto", 
+    width: "300px",
+    border: "1px solid #ccc",
+    borderRadius: "8px",
+    padding: "16px",
+    boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
+    backgroundColor: "#fff",
+  };
+
 const estiloImagem = {
     width: "300px"
 }
 
     return <><h1>Filmes</h1>
+
+
 
     <ul>
         <li>
