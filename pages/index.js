4cfd7f266923ee8ce8e707629cccfd4eb33a0c4f
Anna SS+ <saninascimento2006@gmail.com>
2025-10-13 22:04:52 -0300

    adfsdhtdtufyj

--- pages/index.js
+++ pages/index.js
@@ -33,54 +33,19 @@ const estiloImagem = {
 
 
     <ul>
-        <li>
-            <p>Título: {filmes[0].titulo}</p>
-            <p>Ano: {filmes[0].ano}</p>
-            <p>Gênero: {filmes[0].genero}</p>
-            <p>Nota: {filmes[0].nota}</p>
-
-            <img style= {estiloImagem} src= {filmes[0].poster}/>
-        </li>    
-        <li>
-            <p>Título: {filmes[1].titulo}</p>
-            <p>Ano: {filmes[1].ano}</p>
-            <p>Gênero: {filmes[1].genero}</p>
-            <p>Nota: {filmes[1].nota}</p>
-
-            <img style= {estiloImagem} src= {filmes[1].poster}/>
-        </li> 
-        <li>
-            <p>Título: {filmes[2].titulo}</p>
-            <p>Ano: {filmes[2].ano}</p>
-            <p>Gênero: {filmes[2].genero}</p>
-            <p>Nota: {filmes[2].nota}</p>
-
-            <img style= {estiloImagem} src= {filmes[2].poster}/>
-        </li> 
-        <li>
-            <p>Título: {filmes[3].titulo}</p>
-            <p>Ano: {filmes[3].ano}</p>
-            <p>Gênero: {filmes[3].genero}</p>
-            <p>Nota: {filmes[3].nota}</p>
-
-            <img style= {estiloImagem} src= {filmes[3].poster}/>
-            <li>
-            <p>Título: {filmes[4].titulo}</p>
-            <p>Ano: {filmes[4].ano}</p>
-            <p>Gênero: {filmes[4].genero}</p>
-            <p>Nota: {filmes[4].nota}</p>
-
-            <img style= {estiloImagem} src= {filmes[4].poster}/>
-        </li> 
-        <li>
-            <p>Título: {filmes[5].titulo}</p>
-            <p>Ano: {filmes[5].ano}</p>
-            <p>Gênero: {filmes[5].genero}</p>
-            <p>Nota: {filmes[5].nota}</p>
-
-            <img style= {estiloImagem} src= {filmes[5].poster}/>
-        </li> 
-        </li> 
+        {
+            filmes.map((filme) =>{
+                return ( <li key={`card-filmes-${filme.id}`}>
+                            <p>Título: {filme.titulo}</p>
+                            <p>Ano: {filme.ano}</p>
+                            <p>Gênero: {filme.genero}</p>
+                            <p>Nota: {filme.nota}</p>
+                
+                            <img style= {estiloImagem} src= {filme.poster}/>
+                        </li>    
+                );
+            })
+        }
     </ul></>
 }
 export default Home;
